dd8634fc5b027e8807859da2f6836a5a789e9832
Mujtaba Hussain <developer@khalida-apps.com>
2013-04-05 19:11:40 +1100

    MapKit framework and tidy up names.

---
+++ halalious/KAMapViewController.m
@@ -0,0 +1,38 @@
+//
+//  KAMapViewController.m
+//  halalious
+//
+//  Created by Mujtaba Hussain on 5/04/13.
+//  Copyright (c) 2013 Khalida Apps. All rights reserved.
+//
+
+#import "KAMapViewController.h"
+
+@interface KAMapViewController ()
+
+@end
+
+@implementation KAMapViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ halalious/KAViewController.h
@@ -0,0 +1,13 @@
+//
+//  KAMapViewController.h
+//  halalious
+//
+//  Created by Mujtaba Hussain on 5/04/13.
+//  Copyright (c) 2013 Khalida Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KAMapViewController : UIViewController
+
+@end
